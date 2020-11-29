6c47ab5b062dd0fa3e61c61bd6c08533c87ae671
Barin Britva <barinbritva@outlook.com>
2020-11-29 17:31:01 +0300

    Prepare project to further actions.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,42 @@
-// const dataFromUser = getDataFromUser();
-// console.log(dataFromUser);
-
-function sayHello() {
+function sayHello(): void {
     alert('Hello');
 }
 
-function getDataFromUser(message) {
+function getDataFromUser(message: string): string | null {
     message = message === undefined ? 'введи что-нибудь' : message;
     return prompt(message);
 }
 
-function getRandomNumber (min, max) {
+function getRandomNumber (min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// const lButton = document.querySelector('.left-button');
-// lButton.onclick = leftSlide;
-
-const slidesContainerElement = document.querySelector('section.slider .slides-container');
-const slidesCount = slidesContainerElement.children.length;
+const slidesContainerElement = document.querySelector<HTMLDivElement>('section.slider .slides-container');
+const slidesCount = slidesContainerElement?.children.length || 0;
 let currentSlideIndex = 0;
-function doSlide(direction) {
+
+type Direction = -1 | 1;
+
+function doSlide(direction: Direction): void {
+    if (slidesContainerElement == null) {
+        return;
+    }
+
     if (currentSlideIndex === 0 && direction === -1 || 
         currentSlideIndex === slidesCount - 1 && direction === 1) {
         return;
     }
+
     currentSlideIndex += direction;
     const transformationValue = -100 * currentSlideIndex;
+
     slidesContainerElement.style.transform = `translateX(${transformationValue}vw)`;
 }
 
-function leftSlide() {
+function leftSlide(): void {
     doSlide(-1);
 }
 
-function rightSlide() {
+function rightSlide(): void {
     doSlide(1);
 }
