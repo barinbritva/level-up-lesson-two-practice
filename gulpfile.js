9b58530457f5bae867dae58b9439d93c48543c31
Barin Britva <barinbritva@outlook.com>
2020-11-29 23:24:50 +0300

    Add image styles compiling and static files copying.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 const gulp = require('gulp');
 const changed = require('gulp-changed');
+const gulpCopy = require('gulp-copy');
 const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
 const del = require('del');
+
+sass.compiler = require('node-sass');
 
 const paths = {
     images: {
         src: 'static/img/**/*.{jpg,jpeg,png,svg}',
         dist: 'public/img'
+    },
+    styles: {
+        src: 'static/css/**/*.scss',
+        dist: 'public/css'
+    },
+    vendorStyles: {
+        src: 'static/css/**/*.css',
+        dist: 'public/css'
+    },
+    fonts: {
+        src: 'static/webfonts/**/*.{eot,svg,ttf,woff,woff2}',
+        dist: 'public/webfonts',
     }
 };
 
@@ -23,20 +39,42 @@ function minifyImages() {
                 ]
             })
         ]))
-        .pipe(gulp.dest(paths.images.dist))
-};
+        .pipe(gulp.dest(paths.images.dist));
+}
+
+function compileStyles() {
+    return gulp.src(paths.styles.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.styles.dist));
+}
+
+function copyVendorStyles() {
+    return gulp.src(paths.vendorStyles.src)
+    .pipe(gulp.dest(paths.vendorStyles.dist))
+}
+
+function copyFonts() {
+    return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dist))
+}
 
 function clean() {
-    return del([paths.images.dist]);
-};
+    return del([paths.images.dist, paths.styles.dist]);
+}
 
 function watch() {
     gulp.watch(paths.images.src, gulp.parallel(minifyImages));
+    gulp.watch(paths.styles.src, gulp.parallel(compileStyles));
+    gulp.watch(paths.vendorStyles.src, gulp.parallel(copyVendorStyles));
+    gulp.watch(paths.fonts.src, gulp.parallel(copyFonts));
 }
 
-const build = gulp.series(clean, gulp.parallel(minifyImages));
+const build = gulp.series(clean, gulp.parallel(minifyImages, compileStyles, copyVendorStyles, copyFonts));
 
 exports.minifyImages = minifyImages;
+exports.compileStyles = compileStyles;
+exports.copyVendorStyles = copyVendorStyles;
+exports.copyFonts = copyFonts;
 exports.clean = clean;
 exports.watch = watch;
 exports.default = build;
